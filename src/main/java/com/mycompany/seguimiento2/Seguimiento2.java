2e12c382ac1d04800541d264b6a73596f9436944
Morde <suarez.johan.3695@eam.edu.co>
2026-09-10 11:02:49 -0500

    Haga el pull al repositorio

---
+++ src/main/java/com/mycompany/seguimiento2/Seguimiento2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seguimiento2;
+
+/**
+ *
+ * @author morde
+ */
+public class Seguimiento2 {
+
+    public static void main(String[] args) {
+        System.out.println("ola karen!");
+        
+    }
+}
