78bd86f406419a8b3f85efc17e5fcad4c2423669
karen0820 <>
2026-09-16 12:45:48 -0500

    Actualizacion menu consola

--- src/main/java/main/Seguimiento2.java
+++ src/main/java/main/Seguimiento2.java
@@ -3,15 +3,94 @@
  */
 
 package main;
+import Controladores.GestorCliente;
+import Modelos.Cliente;
+
+import java.util.Scanner;
 
 /**
  *
  * @author morde
  */
 public class Seguimiento2 {
-
     public static void main(String[] args) {
-        System.out.println("ola karen!");
-        
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = leerOpcion();
+            procesarOpcion(opcion);
+        } while (opcion == 0);
+
+        System.out.println("Saliendo del sistema. ¡Hasta luego!");
+        scanner.close();
+    }
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static GestorCliente gestorClientes = new GestorCliente();
+
+
+    private static void mostrarMenu() {
+        System.out.println(""" 
+        ===== MENÚ PRINCIPAL - Biblioteca LibrosYMas =====
+        --- Gestión de Libros ---
+        1. Registrar libro
+        2. Listar libros
+       --- Gestión de Clientes ---
+        3. Registrar cliente
+        5. Mostrar información de un cliente
+        --- Préstamos y Devoluciones ---
+        6. Registrar préstamo
+        7. Registrar devolución
+        0. Salir
+        Seleccione una opción: 
+        """);
+
+    }
+
+    private static int leerOpcion() {
+        try {
+            return Integer.parseInt(scanner.nextLine().trim());
+        } catch (NumberFormatException e) {
+            System.out.println("Entrada inválida. Intente de nuevo.");
+            return -1;
+        }
+    }
+
+    private static void procesarOpcion(int opcion) {
+        int opcionS = scanner.nextInt();
+        switch (opcionS) {
+            case 1:
+
+
+
+                break;              // Johan
+            case 2:
+                System.out.println("ola");;
+                break;    // Johan
+            case 3:
+                System.out.println("ola");
+                break;
+            case 4:
+                System.out.println("ola");
+                break;
+            case 5:
+                System.out.println("ola");
+                break;
+            case 6:
+                System.out.println("ola");
+                break;
+            case 7:
+                System.out.println("ola");
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Opción no válida. Intente de nuevo.");
+        }
+
+    }
+
+    public static void agregarCliente () {
+
     }
 }
